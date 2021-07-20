f33882edd7538efd12487c5197a4fc6a530abc53
zared <zarednkuna8@gmail.com>
2021-07-20 18:39:26 +0200

    enhanced process flow

--- src/main/java/za/co/investec/batch/InvestecAvsBatchApplication.java
+++ src/main/java/za/co/investec/batch/InvestecAvsBatchApplication.java
@@ -7,6 +7,7 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -25,6 +26,69 @@ public class InvestecAvsBatchApplication {
     @Autowired
     public StepBuilderFactory stepBuilderFactory;
 
+    @Bean
+    public JobExecutionDecider decider(){
+        return new DeliveryDecider();
+    };
+
+    @Bean
+    public JobExecutionDecider invoiceDecider(){
+        return new InvoiceDecider();
+    };
+    @Bean
+    public Step giveCustomerRefundStep() {
+        return stepBuilderFactory.get("giveCustomerRefundStep").tasklet(new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println(String.format("Here is your refund."));
+                return RepeatStatus.FINISHED;
+            }
+        }).build();
+    }
+
+    @Bean
+    public Step thankCustomerStep() {
+        return stepBuilderFactory.get("thankCustomerStep").tasklet(new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println(String.format("Thank you for your service."));
+                return RepeatStatus.FINISHED;
+            }
+        }).build();
+    }
+    @Bean
+    public Step verifyPackageIsCorrectWithCustomer() {
+        return stepBuilderFactory.get("verifyPackageIsCorrectWithCustomer").tasklet(new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println(String.format("Check this is the package the customer ordered."));
+                return RepeatStatus.FINISHED;
+            }
+        }).build();
+    }
+
+    @Bean
+    public Step leaveAtDoorStep() {
+        return stepBuilderFactory.get("leaveAtDoorStep").tasklet(new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println(String.format("Leaving package at the door."));
+                return RepeatStatus.FINISHED;
+            }
+        }).build();
+    }
+
+    @Bean
+    public Step storePackageStep() {
+        return stepBuilderFactory.get("storePackageStep").tasklet(new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+
+                System.out.println(String.format("Storing the package while the customer address is located."));
+                return RepeatStatus.FINISHED;
+            }
+        }).build();
+    }
 
     @Bean
     public Step givePackageToCustomerStep() {
@@ -37,8 +101,9 @@ public class InvestecAvsBatchApplication {
             }
         }).build();
     }
+    @Bean
     public Step driveToAddressStep(){
-        Boolean GOT_LOST = false;
+        Boolean GOT_LOST = true;
         return this.stepBuilderFactory.get("driveToAddressStep").tasklet(new Tasklet() {
             @Override
             public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
@@ -66,11 +131,20 @@ public class InvestecAvsBatchApplication {
 
     @Bean
     public Job deliverPackageJob() {
-        return jobBuilderFactory.get("deliverPackageJob")
-            .start(packageItemStep())
-            .next(driveToAddressStep())
-            .next(givePackageToCustomerStep())
-            .build();
+        return this.jobBuilderFactory.get("deliverPackageJob")
+                .start(packageItemStep())
+                .next(driveToAddressStep())
+                    .on("FAILED").stop()
+                .from(driveToAddressStep())
+                    .on("*").to(decider())
+                    .on("PRESENT").to(givePackageToCustomerStep())
+                   .from(verifyPackageIsCorrectWithCustomer())
+                       .next(invoiceDecider()).on("CORRECT").to(thankCustomerStep())
+                       .from(invoiceDecider()).on("INCORRECT").to(giveCustomerRefundStep())
+                    .from(decider())
+                        .on("NOT PRESENT").to(leaveAtDoorStep())
+                .end()
+                .build();
     }
 
 
